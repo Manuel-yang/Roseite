70e22186b70bff6ece722602d77f0b540533c39a
yanaemons <731270812@qq.com>
2023-09-19 10:24:41 +0800

    update

--- programs/nft_social_media/src/instructions/counter.rs
+++ programs/nft_social_media/src/instructions/counter.rs
@@ -2,25 +2,26 @@ use crate::{errors::ErrorCode, state::ProgramPda};
 use anchor_lang::prelude::*;
 use serde_json::json;
 
-pub fn increase(ctx: Context<Increase>) -> Result<()> {
+pub fn increase(ctx: Context<Increase>, test:String) -> Result<()> {
     // only admin can invoke this function
-    require!(
-        ctx.accounts.payer.key() == ctx.accounts.program_admin_pda.admin,
-        ErrorCode::InvalidAuthor
-    );
+    msg!("{}",test);
+    // require!(
+    //     ctx.accounts.payer.key() == ctx.accounts.program_admin_pda.admin,
+    //     ErrorCode::InvalidAuthor
+    // );
 
-    let counter_account = &mut ctx.accounts.counter_account;
-    if counter_account.authority != ctx.accounts.user.key() {
-        counter_account.authority = ctx.accounts.user.key()
-    }
-    let current_count = &counter_account.count;
-    counter_account.count = if u64::MAX - current_count >= 1 {
-        current_count.checked_add(1).unwrap()
-    } else {
-        u64::MAX
-    };
-    let log = json!({"Func":"addMintTime", "userAddress": ctx.accounts.user.key().to_string(),"currentMintTime": counter_account.count.to_string()});
-    msg!("{}", serde_json::to_string_pretty(&log).unwrap());
+    // let counter_account = &mut ctx.accounts.counter_account;
+    // if counter_account.authority != ctx.accounts.user.key() {
+    //     counter_account.authority = ctx.accounts.user.key()
+    // }
+    // let current_count = &counter_account.count;
+    // counter_account.count = if u64::MAX - current_count >= 1 {
+    //     current_count.checked_add(1).unwrap()
+    // } else {
+    //     u64::MAX
+    // };
+    // let log = json!({"Func":"addMintTime", "userAddress": ctx.accounts.user.key().to_string(),"currentMintTime": counter_account.count.to_string()});
+    // msg!("{}", serde_json::to_string_pretty(&log).unwrap());
     Ok(())
 }
 
