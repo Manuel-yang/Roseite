00480c86a4986d478cd6066a3c458fc40dcd5f47
yanaemons <731270812@qq.com>
2023-10-05 19:13:49 +0800

    feat : get nft config account

--- app/src/contexts/NftAccountContext.tsx
+++ app/src/contexts/NftAccountContext.tsx
@@ -4,7 +4,15 @@ import useNftScanner from "../hooks/useNftScanner";
 import { nftInfo } from "./NftScannerContext";
 import { getNftConfigPda } from "../utils/pdas";
 import { PublicKey } from "@solana/web3.js";
-import { createProgram } from "../utils/utils";
+import BN from "bn.js";
+
+export type nftConfigPdaAccount = {
+  bump: number,
+  fansNum: BN,
+  nftCurrentHolder: PublicKey,
+  nftMint: PublicKey,
+  postsNum: BN
+}
 
 interface NftAccountState {
   // selectedNftAccount: nftInfo,
@@ -15,18 +23,26 @@ export function NftAccountProvidr({ children }: { children: ReactNode }) {
     const workspace = useWorkspace()
     const NftScanner = useNftScanner()
     const [selectedNft, setSelectedNft] = useState<nftInfo>(null!)
-    // const program = createProgram()
+    const [nftConfigPdaAccount, setNftConfigPdaAccount] = useState<nftConfigPdaAccount>()
+    
     useEffect(() => {
-      if(NftScanner.selectedNftId) {
+      if(NftScanner.selectedNftId && workspace) {
+        const program = workspace.program
         setSelectedNft(NftScanner.nftsList[NftScanner.selectedNftId])
         if(selectedNft) {
           getNftConfigPda(new PublicKey(selectedNft.mint)).then((res) => {
-            // console.log(res[0].toBase58())
-            console.log(workspace)
+            program.account.nftConfigPda.fetch(res[0].toBase58())
+              .then((res) => {
+                setNftConfigPdaAccount(res)
+              })
           })
         }
       }
-    })
+    },[selectedNft ,NftScanner])
+    if(nftConfigPdaAccount) {
+      console.log(nftConfigPdaAccount)
+      // console.log((nftConfigPdaAccount.postsNum).toNumber())
+    }
 
     return <NftAccountContext.Provider value="">{children}</NftAccountContext.Provider>
 }
