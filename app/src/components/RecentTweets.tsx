069be962c68070040848aaa916d517ef0d44d241
H-Blues <hblues419@gmail.com>
2023-10-13 02:39:55 +0100

    feat: add expanded comments component

--- app/src/components/RecentTweets.tsx
+++ app/src/components/RecentTweets.tsx
@@ -10,32 +10,21 @@ export default function RecentTweets({ tweets }: { tweets: Tweet[] }) {
     <div className="h-full overflow-y-auto pl-2">
       <ol className="relative border-l border-skin-primary">
         {tweets.map((tweet, i) => {
-          const userRoute =
-            tweet.user.toBase58() === owner
-              ? "/profile"
-              : `/users/${tweet.user.toBase58()}`;
+          const userRoute = tweet.user.toBase58() === owner ? "/profile" : `/users/${tweet.user.toBase58()}`;
           return (
             <li key={i} className="ml-4 mb-4">
               <div className="absolute w-3 h-3 bg-fill-third rounded-full mt-1.5 -left-1.5"></div>
               <div className="relative mb-4 text-left">
                 <time className="mb-1 leading-none">
-                  <Link href={`/tweets/${tweet.key}`}>
-                    <a className="text-sm text-color-third hover:underline">
-                      {tweet.created_ago}
-                    </a>
-                  </Link>
+                  <a className="text-sm text-color-third hover:underline">{tweet.created_ago}</a>
                 </time>
                 <div>
                   <Link href={userRoute}>
-                    <a className="font-bold hover:underline text-color-primary">
-                      {tweet.user_display}
-                    </a>
+                    <a className="font-bold hover:underline text-color-primary">{tweet.user_display}</a>
                   </Link>
                   <span className="text-color-third ml-1">posted</span>
                 </div>
-                <div className="truncate text-color-secondary">
-                  {tweet.content}
-                </div>
+                <div className="truncate text-color-secondary">{tweet.content}</div>
                 <div>
                   {/* <Link href={`/tags/${tweet.tag}`}>
                     <a className="text-primary-500 transition-colors hover:underline">
--- app/src/components/UserTweetCard.tsx
+++ app/src/components/UserTweetCard.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { UserTweet } from "../models";
-import { FaRegComment, FaHeart, FaRetweet } from "react-icons/fa";
+import { FaRegComment, FaHeart, FaRetweet, FaAngleDoubleDown, FaAngleDoubleUp } from "react-icons/fa";
 import useTheme from "../hooks/useTheme";
 import useNftScanner from "../hooks/useNftScanner";
 import CopyToClipboardButton from "./CopyButton";
-import { bnToData } from "../utils";
-import { set } from "@metaplex-foundation/umi/serializers";
+import { bnToData, toLongCollapse } from "../utils";
+import CommentModal from "./CommentModal";
+import CommentExpandCard from "./CommentExpandCard";
 
 export default function UserTweetCard({ tweet }: { tweet: UserTweet }) {
   const { theme } = useTheme();
   const { nftsList, selectedNftId } = useNftScanner();
+
   const [retweets, setRetweets] = useState(0);
   const [likes, setLikes] = useState(0);
   const [comments, setComments] = useState([]);
+
   const [retweetColor, setRetweetColor] = useState("");
   const [commentColor, setCommentColor] = useState("");
   const [likesColor, setLikesColor] = useState("");
+
+  const [commentOpen, setCommentOpen] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const changeCommentColor = () => {
     if (commentColor === "yellow") {
@@ -25,24 +31,38 @@ export default function UserTweetCard({ tweet }: { tweet: UserTweet }) {
     }
   };
 
+  const handleCommentOpen = () => {
+    setCommentOpen(!commentOpen);
+  };
+
+  const handleIsExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className={`card py-4 px-8 ${theme === "dark" ? "dark" : ""}`}>
+    <div className={`card py-4 px-16 ${theme === "dark" ? "dark" : ""}`}>
       <div className="card-content">
-        <div className="flex items-center mb-4">
+        {/* Card Header */}
+        <div className="flex items-center mb-4 mt-3">
           {selectedNftId !== undefined ? (
             <img className="w-10 h-11 rounded-full mr-4" src={nftsList[selectedNftId]?.data.metadata.image} />
           ) : (
             <img className="w-10 rounded-full" src={`https://avatars.dicebear.com/api/jdenticon/undefined.svg`} />
           )}
           {selectedNftId !== undefined && (
-            <p className="text-gray-500 mr-3">{`${
-              nftsList[selectedNftId]?.data.name
-            } @${tweet.nftAddress.toString()}`}</p>
+            <>
+              <span className="font-medium text-gray-500 mr-4 ">{`${nftsList[selectedNftId]?.data.name}`} </span>
+              <span className="text-gray-500 mr-3"> {`@${toLongCollapse(tweet.nftAddress)}`}</span>
+            </>
           )}
           <CopyToClipboardButton textToCopy={tweet.nftAddress.toString()} />
         </div>
+
+        {/* Card Content */}
         <p className={`text-lg mb-4 ${theme === "dark" ? "text-white" : ""}`}>{tweet.content}</p>
         <p className="text-gray-500 mb-1">{bnToData(tweet.timeStamp)}</p>
+
+        {/* Card Footer */}
         <div className="flex space-x-8 items-center mb-2">
           <div className="flex items-center">
             <FaRetweet className="text-gray-500 mr-1" color={retweetColor} />
@@ -63,17 +83,6 @@ export default function UserTweetCard({ tweet }: { tweet: UserTweet }) {
             </span>
           </div>
           <div className="flex items-center">
-            <FaRegComment className="text-gray-500 mr-1" color={commentColor} />
-            <span className="text-gray-500 mr-1">{tweet.reviewNum.toString()} </span>
-            <span
-              className="text-gray-500 cursor-pointer hover:underline"
-              onMouseEnter={changeCommentColor}
-              onMouseLeave={changeCommentColor}
-            >
-              Comments
-            </span>
-          </div>
-          <div className="flex items-center">
             <FaHeart className="text-gray-500 mr-1" color={likesColor} />
             <span className="text-gray-500 mr-1">{likes} </span>
             <span
@@ -91,14 +100,43 @@ export default function UserTweetCard({ tweet }: { tweet: UserTweet }) {
               Likes
             </span>
           </div>
+          <div className="flex items-center">
+            <CommentModal
+              tweet={tweet}
+              isOpen={commentOpen}
+              comments={comments}
+              setComments={setComments}
+              onClose={handleCommentOpen}
+            />
+            <FaRegComment className="text-gray-500 mr-1" color={commentColor} />
+            <span className="text-gray-500 mr-1">{comments.length} </span>
+            <span
+              className="text-gray-500 cursor-pointer hover:underline"
+              onMouseEnter={changeCommentColor}
+              onMouseLeave={changeCommentColor}
+              onClick={handleCommentOpen}
+            >
+              Comments
+            </span>
+            <button>
+              {isExpanded ? (
+                <FaAngleDoubleUp
+                  className="ml-1 mt-1"
+                  color={`${theme === "dark" ? "gray" : "black"}`}
+                  onClick={handleIsExpanded}
+                />
+              ) : (
+                <FaAngleDoubleDown
+                  className="ml-1 mt-1"
+                  color={`${theme === "dark" ? "gray" : "black"}`}
+                  onClick={handleIsExpanded}
+                />
+              )}
+            </button>
+          </div>
         </div>
-        {/* <button
-        className="flex items-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mt-4 rounded"
-        onClick={() => onDelete(tweet)}
-      >
-        <FaTrash className="text-white mr-1" />
-        Delete
-      </button> */}
+
+        {isExpanded ? <CommentExpandCard comments={comments} /> : null}
       </div>
     </div>
   );
