213931df23a33acbc7d91e60688049735bb7b994
yanaemons <731270812@qq.com>
2023-10-18 17:35:55 +0800

    feat : add process ui after deleting post

--- app/src/components/UserTweetMenubar.tsx
+++ app/src/components/UserTweetMenubar.tsx
@@ -9,11 +9,24 @@ import {
   import '@szhsin/react-menu/dist/index.css';
 import useTweets from '../hooks/useTweets';
 import { BN } from '@project-serum/anchor';
+import theme from 'flowbite-react/lib/esm/theme/default';
+import { notifyLoading, notifyUpdate } from '../utils';
+import JSConfetti from 'js-confetti';
+import useTheme from '../hooks/useTheme';
   
   export default function UserTweetMenubar({nftMintAddress, postPdaAddress, postId} : {nftMintAddress: PublicKey, postPdaAddress: PublicKey, postId: BN}) {
+    const jsConfetti = new JSConfetti();
     const { deleteTweet } = useTweets()
-    const deletePost = () => {
-      deleteTweet(nftMintAddress, postPdaAddress, postId)
+    const { theme } = useTheme();
+    
+    const deletePost = async () => {
+      const toastId = notifyLoading("Transaction in progress. Please wait...", theme);
+      jsConfetti.addConfetti({
+        emojiSize: 20,
+        confettiNumber: 120,
+      });
+      const result = await deleteTweet(nftMintAddress, postPdaAddress, postId)
+      notifyUpdate(toastId, result.message, result.success ? "success" : "error");
     }
     return (
       <Menu menuButton={<MenuButton className="text-white text-lg">...</MenuButton>}>
