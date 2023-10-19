5d0f8bb71bc9ef926c057d8c120a3f1bba73047c
yanaemons <731270812@qq.com>
2023-10-19 16:15:08 +0800

    style: change the user card style

--- app/src/components/UserTweetMenubar.tsx
+++ app/src/components/UserTweetMenubar.tsx
@@ -9,10 +9,11 @@ import {
   import '@szhsin/react-menu/dist/index.css';
 import useTweets from '../hooks/useTweets';
 import { BN } from '@project-serum/anchor';
-import theme from 'flowbite-react/lib/esm/theme/default';
 import { notifyLoading, notifyUpdate } from '../utils';
 import JSConfetti from 'js-confetti';
 import useTheme from '../hooks/useTheme';
+import { Button, Dropdown } from "flowbite-react";
+import { HiDotsVertical } from 'react-icons/hi';
   
   export default function UserTweetMenubar({nftMintAddress, postPdaAddress, postId} : {nftMintAddress: PublicKey, postPdaAddress: PublicKey, postId: BN}) {
     const jsConfetti = new JSConfetti();
@@ -23,15 +24,23 @@ import useTheme from '../hooks/useTheme';
       const toastId = notifyLoading("Transaction in progress. Please wait...", theme);
       const result = await deleteTweet(nftMintAddress, postPdaAddress, postId)
       notifyUpdate(toastId, result.message, result.success ? "success" : "error");
-      jsConfetti.addConfetti({
-        emojiSize: 20,
-        confettiNumber: 120,
-      });
+      if(result.success) {
+        jsConfetti.addConfetti({
+          emojiSize: 20,
+          confettiNumber: 120,
+        });
+      }
     }
     return (
-      <Menu menuButton={<MenuButton className="text-white text-lg">...</MenuButton>}>
-        <MenuItem className="text-white" onClick={deletePost}>Delete</MenuItem>
-      </Menu>
+      <Dropdown 
+        label=""  
+        renderTrigger={() => 
+        <Button size="xs" color='#191c29'>
+          <HiDotsVertical color='#14F195'/>
+        </Button>}>
+        <Dropdown.Item onClick={deletePost}>Delete</Dropdown.Item>
+
+      </Dropdown>
     );
   }
   
